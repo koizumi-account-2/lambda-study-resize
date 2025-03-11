797317a399071d24af84f619b830422f674c5694
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-03-12 00:58:34 +0900

    演習-2(出力変更)

--- lambdas/test/src/index.ts
+++ lambdas/test/src/index.ts
@@ -1,6 +1,11 @@
 import {S3Event, S3Handler} from 'aws-lambda'
  
- export const handler:S3Handler = (event:S3Event)=>{
-     console.log("呼ばれました");
-     console.log('%o',event)
- }
+export const handler:S3Handler = (event:S3Event)=>{
+    let fileName = '';
+    let bucketName = '';
+    for(const record of event.Records){ 
+        fileName = record?.s3?.object?.key;
+        bucketName = record?.s3?.bucket?.name;
+        console.log(fileName,bucketName,`${bucketName}に${fileName}がuploadされました。`);
+    }
+}
